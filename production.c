6b6c1168b0182b492f79269d5786a19d8a886987
Mohan <mohan252@gmail.com>
2016-12-23 09:04:54 -0500

    working fine

--- production.c
+++ production.c
@@ -3,34 +3,24 @@
 
 char auth[] = "e3ac4c915633434dbcb322fd5587f501";
 const int relay = D3;
-const int reedSwitch = D0;
 SparkCorePolledTimer updateTimer(20000); //Create a timer object and set it's timeout in milliseconds
 int timerCount = 0;
-int highCount = 0;
 int howLongCheckToGetStableReadingInMin = 5 * 3; // 15 20ms interval, // this will make door to open for 15 * 20 = 300 secs = 5 min
-void (*action)(String) = NULL;
-void (*nextAction)(String) = NULL;
+int (*action)(String) = NULL;
 
 void setup() {
     Serial.begin(9600);
     Blynk.begin(auth);
     pinMode(relay, OUTPUT);
-    pinMode(reedSwitch, INPUT); 
     
-    Particle.function("newaction", activateDoor);
-    Particle.function("doorStatus", doorStatus);
+    Particle.function("activateDoor", activateDoor);
     Particle.function("openClose", openClose);
 }
 
-void triggerActionAfterIntervalElapsed(int noOfIntervalsToWait, void (*actionToExecute)(String)){
+void triggerActionAfterIntervalElapsed(int noOfIntervalsToWait, int (*actionToExecute)(String)){
     action = actionToExecute;
     howLongCheckToGetStableReadingInMin = noOfIntervalsToWait; // each interval is 20 ms
     triggerTimerToTakeAction(); 
-}
-
-int doorStatus(String args){
-    int minToWait = args.toInt();
-    triggerActionAfterIntervalElapsed(minToWait * 3,&publishStatus);
 }
 
 int activateDoor(String args){
@@ -43,27 +33,7 @@ int activateDoor(String args){
 int openClose(String args){
     int minToWait = args.toInt();
     activateDoor(args);
-    triggerActionAfterIntervalElapsed(minToWait * 3,&closeDoorIfOpen);
-}
-
-void closeDoorIfOpen(String args){
-    if(highCount > 0){
-        activateDoor(args);
-        delay(60000);
-    }
-    nextAction = &publishStatus;
-    // triggerActionAfterIntervalElapsed(6,&publishStatus);
-}
-
-void publishStatus(String args){
-    action = NULL;
-    if(highCount > 0){
-        Particle.publish("ifttt", "open");
-    }
-    else{
-        Particle.publish("ifttt", "close");
-    }
-    nextAction = NULL;
+    triggerActionAfterIntervalElapsed(minToWait * 3,&activateDoor);
 }
 
 void triggerTimerToTakeAction(){
@@ -72,10 +42,6 @@ void triggerTimerToTakeAction(){
 
 void timerCheck(){
     timerCount = timerCount + 1;
-    if(digitalRead(reedSwitch) == HIGH)
-    {
-        highCount = highCount + 1;    
-    }
     if(timerCount == howLongCheckToGetStableReadingInMin){ 
         takeActionAndResetTimer("timer");
     }
@@ -86,10 +52,6 @@ void takeActionAndResetTimer(String args){
     action(args);
     action = NULL;
     timerCount = 0;
-    highCount = 0;
-    if(nextAction != NULL){
-        triggerActionAfterIntervalElapsed(6,nextAction);
-    }
 }
 
 
@@ -103,7 +65,7 @@ BLYNK_WRITE(V2) //Button Widget is writing to pin V2
   openClose("2");
 }
 
-BLYNK_WRITE(V3) //Button Widget is writing to pin V2
+BLYNK_WRITE(V5) //Button Widget is writing to pin V2
 {
   openClose("5");
 }
